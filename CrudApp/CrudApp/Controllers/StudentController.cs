df9e750f38e2402a09e4be9f2a3ca8486627a6d0
Mamun ahmed <Mamunahmedt@outlook.com>
2020-10-19 10:06:12 +0600

    CrudwithapiGuiVersion

---
+++ CrudApp/CrudApp/Controllers/StudentController.cs
@@ -0,0 +1,130 @@
+﻿using CrudApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace CrudApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+
+            List<Student> students = new List<Student>();
+            students = (List<Student>)getAllStudent();
+            return View("ViewStudent", students);
+        }
+
+        public IActionResult CreateStudent()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            if (student.StudentID == 0)
+            {
+                string query = "Insert Into Studentinfo values('" + student.StudentName + "','" + student.Address + "','" + student.Phone + "','" + student.Email + "' )";
+                SqlConnection conn = new SqlConnection(@"data source=DESKTOP-40RFCPG\MAMUNSQL; initial catalog=DotNetCoreCrudDB; integrated security=true;");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                int result = cmd.ExecuteNonQuery();
+                conn.Close();
+                if (result > 0)
+                {
+                    return RedirectToAction("Index");
+
+                }
+
+                return NotFound("Failed to save");
+
+            }
+            else
+            {
+                string query = "Update Studentinfo set StudentName='" + student.StudentName + "',Address='" + student.Address + "',Phone='" + student.Phone + "',Email='" + student.Email + "' where StudentID='" + student.StudentID + "'";
+                SqlConnection conn = new SqlConnection(@"data source=DESKTOP-40RFCPG\MAMUNSQL; initial catalog=DotNetCoreCrudDB; integrated security=true;");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                int result = cmd.ExecuteNonQuery();
+                conn.Close();
+                if (result > 0)
+                {
+                   
+                    return RedirectToAction("Index");
+
+                }
+
+            }
+            return NotFound();
+        }
+
+        public IEnumerable<Student> getAllStudent()
+        {
+            {
+                List<Student> students = new List<Student>();
+                string query = "select * from StudentInfo";
+                SqlConnection conn = new SqlConnection(@"data source=DESKTOP-40RFCPG\MAMUNSQL; initial catalog=DotNetCoreCrudDB; integrated security=true;");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+
+                    Student student = new Student();
+                    student.StudentID = (int)reader["StudentID"];
+                    student.StudentName = reader["StudentName"].ToString();
+                    student.Address = reader["Address"].ToString();
+                    student.Phone = reader["Phone"].ToString();
+                    student.Email = reader["Email"].ToString();
+
+                    students.Add(student);
+
+
+
+                }
+                conn.Close();
+                return students;
+
+            }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            string query = "select * from StudentInfo where StudentId='"+id+"'";
+            SqlConnection conn = new SqlConnection(@"data source=DESKTOP-40RFCPG\MAMUNSQL; initial catalog=DotNetCoreCrudDB; integrated security=true;");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+
+                Student student = new Student();
+                student.StudentID = (int)reader["StudentID"];
+                student.StudentName = reader["StudentName"].ToString();
+                student.Address = reader["Address"].ToString();
+                student.Phone = reader["Phone"].ToString();
+                student.Email = reader["Email"].ToString();
+
+                return View("CreateStudent",student);
+
+
+
+            }
+            return NotFound();
+
+
+        }
+
+      
+
+      
+
+
+        
+
+    }
+    }
+
---
+++ CrudApp/CrudApp/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApp.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public String StudentName { get; set; }
+        public String Address { get; set; }
+        public String Phone  { get; set; }
+        public String Email  { get; set; }
+
+
+    }
+}
